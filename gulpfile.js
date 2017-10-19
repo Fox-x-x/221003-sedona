f4dab35d8787d1734d7650be8b95ba30bdaf6c6e
Fox-x-x <gemini86@list.ru>
2017-10-19 11:24:51 +0200

    резиновые страницы index, photo для моб. версий

--- gulpfile.js
+++ gulpfile.js
@@ -16,25 +16,6 @@ var run = require("run-sequence");
 var del = require("del");
 
 
-gulp.task("images", function() {
-  return gulp.src("build/img/**/*.{png,jpg,gif}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true})
-  ]))
-  .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("symbols", function() {
-   return gulp.src("build/img/*.svg")
-     .pipe(svgmin())
-     .pipe(svgstore({
-     inlineSvg: true
-   }))
-   .pipe(rename("symbols.svg"))
-   .pipe(gulp.dest("build/img"));
-});
-
 gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
@@ -44,7 +25,7 @@ gulp.task("style", function() {
         "last 2 versions"
       ]}),
       mqpacker({
-        sort: true
+        sort: false
       })
     ]))
     .pipe(gulp.dest("build/css"))
@@ -52,6 +33,15 @@ gulp.task("style", function() {
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("html:copy", function() {
@@ -77,9 +67,13 @@ gulp.task("serve", function() {
   gulp.watch("*.html", ["html:update"]);
 });
 
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", "symbols", fn);
+});
+
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff, woff2}",
+    "fonts/**/*.{woff,woff2}",
     "img/**",
     "js/**",
     "*.html"
@@ -93,6 +87,12 @@ gulp.task("clean", function() {
   return del("build");
 });
 
-gulp.task("build", function(fn) {
-  run("clean", "copy", "style", "images", "symbols", fn);
+gulp.task("symbols", function() {
+   return gulp.src("build/img/*.svg")
+     .pipe(svgmin())
+     .pipe(svgstore({
+        inlineSvg: true
+   }))
+   .pipe(rename("symbols.svg"))
+   .pipe(gulp.dest("build/img"));
 });
